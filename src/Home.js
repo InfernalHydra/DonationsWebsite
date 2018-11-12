59a32f572744d2de33ebfd2597943d8eb688af42
Ryan Chhong <ryanchhong@gmail.com>
2018-11-11 21:01:21 -0600

    Display total amount of donations on home screen

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,34 @@ import React, {Component} from 'react'
 import {withTracker} from 'meteor/react-meteor-data'
 import { Meteor } from 'meteor/meteor'
 import { Acts } from './api/Acts'
+import {Bids} from './api/Bids'
 
 class Home extends Component
 {
     render()
     {
-        if(!this.props.isReady)
+        if(!this.props.isReadyActs && !this.props.isReadyBids)
         {
             return <div>loading</div>;
         }
-        let text = this.props.actInProgress ? "The current act is: " + this.props.currAct.name + " by " + this.props.currAct.author : "";
         return (
             <div id = "home-container" style={{width : '80%', margin: 'auto'}}>
                 <h1>Welcome to Aid the Cause 2018</h1>
                 {Meteor.userId() ? null : <div>Please log in to donate</div>}
+                <div>{this.props.currAct ? "The current act is: " + this.props.currAct.name + " by " + this.props.currAct.author : ""}</div>
+                <div>{this.props.isReadyBids ? "The current total of donations is: $" + this.props.bids.reduce((a,b) => {return a + b.amount}, 0) : ""}</div>
             </div>
         );
     }
 }
 
 export default withTracker(() => {
-    const subsription = Meteor.subscribe('acts');
+    const subsriptionActs = Meteor.subscribe('acts');
+    const subsriptionBids = Meteor.subscribe('bids');
     return {
-        isReady : subsription.ready(),
-        actInProgress : Acts.find({status : "In Progress"}).fetch().length,
-        currAct : Acts.findOne({status : "In Progress"}),
+        isReadyActs : subsriptionActs.ready(),
+        isReadyBids : subsriptionBids.ready(),
+        currAct : Acts.findOne({status : {$ne : "Scheduled"}}),
+        bids: Bids.find({status : "Fufilled"}).fetch(),
     }
 })(Home)
